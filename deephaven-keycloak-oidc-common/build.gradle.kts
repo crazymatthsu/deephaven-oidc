878a22f34f52d8b36de816a80b3092080eaba711
Matt <79299508+crazymatthsu@users.noreply.github.com>
2026-07-27 21:05:42 -0400

    build: add MSAL4J to common module for EntraTokenClient

--- deephaven-keycloak-oidc-common/build.gradle.kts
+++ deephaven-keycloak-oidc-common/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 dependencies {
     api(libs.deephaven.client.barrage)
     implementation(libs.jackson.databind)
+    implementation(libs.msal4j)
     runtimeOnly(libs.deephaven.log.to.slf4j)
     runtimeOnly(libs.slf4j.simple)
 }
